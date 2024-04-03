72532b7811d9385b8314a8f1aa6fce4a63c5c890
theteNikita <145757957+theteNikita@users.noreply.github.com>
2024-04-03 13:39:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// // Get the navbar element
+// const navbar = document.querySelector('.navbar');
+
+// // Get the navbar toggle button element
+// const toggleButton = document.querySelector('.navbar-toggle');
+
+// // Add a click event listener to the toggle button
+// toggleButton.addEventListener('click', function() {
+//   // Toggle the 'responsive' class on the navbar element
+//   navbar.classList.toggle('responsive');
+// });
+
+// Toggle the dropdown content on click for small screens
+
+
